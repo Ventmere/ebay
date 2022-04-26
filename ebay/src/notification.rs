98bdf929a4bcd433d37583f38f86db4b15bd5c63
Flux Xu <fluxxu@gmail.com>
2022-04-26 13:05:32 -0400

    fix: notification challenge

--- ebay/src/notification.rs
+++ ebay/src/notification.rs
@@ -1,9 +1,3 @@
-use hmac_sha256::HMAC;
-
 pub fn get_challenge_response(code: &str, verification_token: &str, endpoint: &str) -> String {
-  let mut hasher = HMAC::new(code.as_bytes());
-  hasher.update(verification_token.as_bytes());
-  hasher.update(endpoint.as_bytes());
-  let bytes = hasher.finalize();
-  hex::encode(&bytes)
+  sha256::digest(format!("{}{}{}", code, verification_token, endpoint))
 }
